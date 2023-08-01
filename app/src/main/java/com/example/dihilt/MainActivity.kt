58ab284059272adeccce2f464041d6c06aeeef03
guemriaida <ayda.guemri@isim.u-gabes.tn>
2023-08-01 13:58:19 +0100

    DI Hilt

--- app/src/main/java/com/example/dihilt/MainActivity.kt
+++ app/src/main/java/com/example/dihilt/MainActivity.kt
@@ -6,7 +6,6 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,19 +14,23 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.ViewModelProvider
-import com.bumptech.glide.Glide
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.example.dihilt.ui.theme.DIHiltTheme
 import com.example.dihilt.viewmodel.DogViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    lateinit var viewModel: DogViewModel
+
         override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setUpViewModel()
+        viewModel.getRandomDogs()
+        setUpObserver()
         setContent {
             DIHiltTheme {
                 // A surface container using the 'background' color from the theme
@@ -38,105 +41,42 @@ class MainActivity : ComponentActivity() {
                             verticalArrangement = Arrangement.Center,
                             horizontalAlignment = Alignment.CenterHorizontally
                         ) {
-                            MyUI()
-                        }
+                            MyUI("https://images.dog.ceo/breeds/doberman/n02107142_10952.jpg")
+                       }
                     }
             }
         }
     }
-        setUpViewModel()
-        viewModel.getRandomDogs()
-        setUpObserver()
+//     setUpObserver()
 }
-    @Composable
-    fun MyUI() {
-        Image(
-
-            painter = painterResource(R.drawable.ic_launcher_foreground),
-            contentDescription = stringResource(id = R.string.dog) ,
-
-        )
-    }
-
-
-//    private lateinit var binding: ActivityMainBinding
-    lateinit var viewModel: DogViewModel
-
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
-//        setUpViewModel()
-//        initViews()
-//        setUpObserver()
-//    }
 
     private fun setUpObserver() {
+
+
         with(viewModel) {
             dogResponse.observe(this@MainActivity) {
                 Log.e(TAG, "setUpObserver 1 : "+ it.message)
-//                Glide.with(this@MainActivity)
-//                    .load(it.message)
-//                    .placeholder(R.drawable.ic_launcher_background)
-//                    .error(android.R.drawable.ic_dialog_alert)
-//                    .into(Id.dog)
+
             }
 
             viewModel.error.observe(this@MainActivity) {
+
                 Log.e(TAG, "setUpObserver 2 : " + it )
                 Toast.makeText(this@MainActivity, it, Toast.LENGTH_LONG).show()
             }
+
+
         }
+
+
     }
 
     private fun setUpViewModel() {
         viewModel = ViewModelProvider(this)[DogViewModel::class.java]
     }
-
-//    private fun initViews() {
-//        binding.btnNewDog.setOnClickListener {
-//            viewModel.getRandomDogs()
-//        }
-//    }
+    @OptIn(ExperimentalGlideComposeApi::class)
+    @Composable
+    fun MyUI(url : String) {
+        GlideImage(model = url, contentDescription =stringResource(id = R.string.dog) )
+    }
 }
-//import android.os.Bundle
-//import androidx.activity.ComponentActivity
-//import androidx.activity.compose.setContent
-//import androidx.compose.foundation.layout.fillMaxSize
-//import androidx.compose.material3.MaterialTheme
-//import androidx.compose.material3.Surface
-//import androidx.compose.material3.Text
-//import androidx.compose.runtime.Composable
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.tooling.preview.Preview
-//import com.example.dihilt.ui.theme.DIHiltTheme
-//
-//class MainActivity : ComponentActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContent {
-//            DIHiltTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-//                    Greeting("Android")
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//            text = "Hello $name!",
-//            modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    DIHiltTheme {
-//        Greeting("Android")
-//    }
-//}
